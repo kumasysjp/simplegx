6756c8f14a1166f8ea89a4b1e9b40f9738171834
kumasysjp <132196840+kumasysjp@users.noreply.github.com>
2023-04-30 18:41:51 +0900

    Add files via upload

---
+++ Test/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports SimpleGXSimu
+
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim ins As New GXSim
+        ins.CPU = enumCPU._Non
+
+        Dim rtn As ReturnUS = ins.ReadDeviceBlock2("D0", 10)
+
+        MsgBox(ins.UShortDis(rtn.dat, "/"))
+
+
+        Dim dat(960) As UShort : For i = 0 To 959 : dat(i) = i + 1 : Next
+
+        ins.WriteDeviceBlock2("D0", 960, dat)
+
+
+
+
+
+
+
+
+    End Sub
+End Class
